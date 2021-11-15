e46e5160822e226daa22484af0659856d669e978
pyrrhus-ich <menzelfrank@gmail.com>
2021-11-15 20:58:02 +0100

    Feinheiten

--- startScript.py
+++ startScript.py
@@ -5,12 +5,12 @@ from skripte.xlsxHandling import readSrcBuHa, bearbBOXlsx, createValList, create
 from skripte.vergleichListen import vergleichListen
 
 
-checkFolders(folderList); #von prüfungen.py prüft ob die Folderstruktur vorhanden ist
-checkFile(workDir, neededFiles) #von prüfungen.py prüft ob source BO und BuHa vorhanden sind
-csvToXlsx(csvSrc) #Kommt von csvHandling und schreibt alle Zeilen aus dem BO csv Bericht in ein Excel File
-srcFileBo = srcFldBo + os.listdir(srcFldBo)[0]  #Liest den Namen des Bo Berichtes und speichert ihn in die Variable
-readSrcBuHa(srcFileBuHa) #Kommt von xlsxHandling, liest die Daten des BuHa Files & speichert diese in einer Liste
-bearbBOXlsx(srcFileBo)#von xlsxHandling.py Baut im BO File die Retourennummer zusammen
-createValList(srcFileBo)#von xlsx Handling erstellt eine Liste mit den zusammengesetzten Retourennummern aus dem Bo xlsx
-vergleichListen(buHaEindeutigeRetourennummer, valList)#xslxHandling.py vergleicht beide Listen und speichert übereinstimmungen
-createDstFile(resultList, dstFile)
+checkFolders(folderList);                                   #prüft ob die Folderstruktur vorhanden ist
+checkFile(workDir, neededFiles)                             #prüft ob source BO und BuHa vorhanden sind
+csvToXlsx(csvSrc)                                           #schreibt alle Zeilen aus dem BO csv Bericht in ein Excel File
+srcFileBo = srcFldBo + os.listdir(srcFldBo)[0]              #Liest den Namen des Bo Berichtes und speichert ihn in die Variable
+readSrcBuHa(srcFileBuHa)                                    #liest die Daten des BuHa Files & speichert diese in einer Liste
+bearbBOXlsx(srcFileBo)                                      #Baut in der  BO xslx Datei die Retourennummer zusammen
+createValList(srcFileBo)                                    #erstellt eine Liste mit den zusammengesetzten Retourennummern aus dem Bo xlsx
+vergleichListen(buHaEindeutigeRetourennummer, valList)      #vergleicht beide Listen und speichert übereinstimmungen
+createDstFile(resultList, dstFile)                          #Erstellt die  endgültige Datei und formatiert sie. 
